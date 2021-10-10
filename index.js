1a418c7ecee55343c4d7efd61d25887ecff750d2
sakib12345 <sakib35-1680@diu.edu.bd>
2021-10-10 15:37:56 +0600

    handleReqRes file create and post data from frontend

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
-const http = require('http');
-const url = require('url');
+//dependensices
+const http = require("http");
+const {handleReqRes} = require("./helpers/handleReqRes");
 
 // app object - module scaffolding
 const app = {};
 
 //configuration
 app.config = {
-    port: 5000,
+  port: 5000,
 };
 
 //create server
 app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, ()=>{
-        console.log(`listening to port ${app.config.port}`);
-    })
-}
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`listening to port ${app.config.port}`);
+  });
+};
 
 //handle request response
-app.handleReqRes = (req, res) => {
-    //request handling
-    // get the url and parse it 
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    const method = req.method.toLowerCase();
-    const quearyStringObject = parsedUrl.query
-
-    console.log(quearyString)
-    //response handle
-    res.end('hello sakib')
-}
+app.handleReqRes = handleReqRes;
 
 //start server
-app.createServer()
+app.createServer();
